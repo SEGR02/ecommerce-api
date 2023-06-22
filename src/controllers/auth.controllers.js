434735d62a1d88024506d3487cbb6f91bcef0cd2
Sebastian <sebastian.gomz02@gmail.com>
2023-06-22 16:04:26 -0400

    feat: bug fixed in register

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,6 +4,18 @@ const transporter = require("../utils/mailer");
 const register = async (req, res, next) => {
   try {
     const user = req.body;
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(400).json({ message: "Not email provided" });
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9-]+\.[A-Z]{2,4}$/i.test(email)) {
+      res.status(400).json({ message: "Incorrect email" });
+    }
+
+    if (!password) {
+      res.status(400).json({ message: "Not password provided" });
+    }
+
     const result = await AuthServices.register(user);
     if (result) {
       res.status(201).json({ message: "user created" });
@@ -11,7 +23,7 @@ const register = async (req, res, next) => {
         to: result.email,
         from: "ian.rosas@academlo.com",
         subjetc: "Email confirmation",
-        html: "<h1>Bienvenido al mejor ecommerceApi creada por mi</h1> <p>Tienes que confirmar tu email</p><p> Solo haz click en el siguiente <a href='#'' target='new_blanc'> enlace </a>",
+        html: "<h1>Bienvenido a la mejor app de streaming</h1> <p>Tienes que confirmar tu email</p><p> Solo haz click en el siguiente <a href='#'' target='new_blanc'> enlace </a>",
       });
     } else {
       res.status(400).json({ message: "something wrong" });
@@ -42,9 +54,9 @@ const login = async (req, res, next) => {
       userData.token = token;
       res.json(userData);
     } else if (result.correctEmail == false) {
-      res.status(400).json({ message: "incorrect email" });
+      res.status(400).json({ message: "Incorrect email" });
     } else {
-      res.status(400).json({ message: "incorrect password" });
+      res.status(400).json({ message: "Incorrect password" });
     }
   } catch (error) {
     next(error);
